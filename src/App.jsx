3705f139ad45e13bb90d9f48e3ef78f88351945e
vings1 <vingfat@gmail.com>
2025-08-16 00:24:29 +0100

    initial commit, setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/all';
+
+gsap.registerPlugin(ScrollTrigger);
+
+const App = () => {
+  return (
+    <main>
+        <div>
+            <h1 className="text-3xl text-pink">Welcome to the world of GTA</h1>
+        </div>
+    </main>
+  )
+}
+
+export default App
