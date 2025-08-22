35ba0da413d363f38fd22e0297e876c8d743734f
vings1 <vingfat@gmail.com>
2025-08-22 16:22:55 +0100

    implement first video

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { ScrollTrigger } from 'gsap/all';
 
 import Hero from './sections/Hero';
 import NavBar from './sections/NavBar';
+import FirstVideo from './sections/FirstVideo';
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -11,6 +12,7 @@ const App = () => {
     <main>
       <NavBar />
       <Hero />
+      <FirstVideo />
     </main>
   )
 }
